9c8f6ec59e9c0ffee4a260f0725377f968d1d23f
Adam Aiken <adam.aiken@gmail.com>
2022-01-17 21:16:14 -0500

    jupyter nb transition started

---
+++ _build/jupyter_execute/chapters/3_0_comp101.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # The Basics
+# 
+# This section covers some ideas that I call **CompSci 101**. These are the sorts of topics that come up in any Intro to Computer Science class. Let's cover the basics, so that we can have some idea of what's going on with our data. 
+# 
+# I can't let you get out of this course without seeing some of this stuff. I'd feel bad. 
+# 
+# 
+# ```{note}
+# You'll find more details in Chapter 3 of our textbook. I am also borrowing heavily from [Chapter 1 of Python for Data Science](https://www.tomasbeuzen.com/python-programming-for-data-science/chapters/chapter1-basics.html)
+# ```
+
+# ## Data types
+# 
+# Computers think of data, or a **value**, as a **type**. For example, in Python, there are three types of numbers: integers, floats, and complex. A **variable** is a name that refers to a value. Python let's you create any variable name as long as it begins with a letter or an underscore, so no numbers to start. It should also not be what is called a r[eserved word](https://docs.python.org/3.3/reference/lexical_analysis.html#keywords) in Python such as `for`, `while`, or `class`. All programming languages have special, reserved words that they don't want us using as variable names. It would get confused.
+# 
+# A common metaphor is to think of a variable as a box that holds some information (like a number, a vector, or a string). We use the **assignment operator** `=` to assign a value to a variable.
+
+# ### Common built-in Python data types
+# 
+# Chapter 3 of our textbook covers the basic data types.
+# 
+# | English name          | Type name  | Type Category  | Description                                   | Example                                    |
+# | :-------------------- | :--------- | :------------- | :-------------------------------------------- | :----------------------------------------- |
+# | integer               | `int`      | Numeric Type   | positive/negative whole numbers               | `42`                                       |
+# | floating point number | `float`    | Numeric Type   | real number in decimal form                   | `3.14159`                                  |
+# | boolean               | `bool`     | Boolean Values | true or false                                 | `True`                                     |
+# | string                | `str`      | Sequence Type  | text                                          | `"I Can Has Cheezburger?"`                 |
+# | list                  | `list`     | Sequence Type  | a collection of objects - mutable & ordered   | `['Ali', 'Xinyi', 'Miriam']`               |
+# | tuple                 | `tuple`    | Sequence Type  | a collection of objects - immutable & ordered | `('Thursday', 6, 9, 2018)`                 |
+# | dictionary            | `dict`     | Mapping Type   | mapping of key-value pairs                    | `{'name':'DSCI', 'code':511, 'credits':2}` |
+# | none                  | `NoneType` | Null Object    | represents no value                           | `None`          
+# 
+# Source: [Chapter 1 of Python for Data Science](https://www.tomasbeuzen.com/python-programming-for-data-science/chapters/chapter1-basics.html)
+
+# [DataCamp tutorial on Python data structures](https://www.datacamp.com/community/tutorials/data-structures-python)
+# 
+# [DataCamp tutorial on Python strings](https://www.datacamp.com/community/tutorials/python-string-tutorial)
+# 
+# ## Writing functions
+# 
+# ```{note}
+# This is just a first look at writing functions. We'll do more later.
+# ```
+# 
+# [DataCamp tutorial](https://www.datacamp.com/community/tutorials/functions-python-tutorial)
+# 
+
+# In[1]:
+
+
+type(23)
+
--- chapters/2_2_vscode.md
+++ chapters/2_2_vscode.md
@@ -1,11 +1,30 @@
 # VS Code
 
+I am new to VS Code. I've used R Studio for years, which is a development environment for the [R statistical language](https://www.r-project.org). Designed for R, by [the folks at R Studio](https://www.rstudio.com). It's a wonderful development environment for my day-to-day research.
+
+I am not much of a programmer by training. I've dabbled for over 25 years in a variety of languages, but with only a few years of combined formal training (but only if [Pascal in 1995 counts](https://en.wikipedia.org/wiki/Pascal_(programming_language))). I'm coming to a lot of these tools with fresh, inexperienced eyes.
+
+That said, I really like VS Code. I'm going to show you the basics, how to navigate it, where to find stuff, how to run your code, how to write Markdown in it, and how to get your output. You can do a lot from this one set of windows. 
+
+And, it is not just for Python. You know Java? Here you go. C/C++, same. You design web pages? Works for that. Want to learn [OCAML](https://ocaml.org) and [become a rich](https://blog.janestreet.com)? Start coding!
+
+
+You can learn more about this popular coding environment on [their web page tutorials](https://code.visualstudio.com/learn). 
 
 ## Using VS Code
 
-[VS Code and Python](https://code.visualstudio.com/docs/languages/python)
+
+```{figure} ../images/02-vs-blank-notebook.png
+---
+height: 400px
+name: 02-vs-blank-notebook
+---
+A blank VS Code Jupyter notebook. You can see my folder structure on the left. Three folders, one for code, one fo data, and one for images (img). 
+```
+
+Visual studio has many helpful tutorials, like this one on using [VS Code with Python](https://code.visualstudio.com/docs/languages/python).
 
 
-### Using Live Share
+### Using Live Share in VS Code
 
-You'll need a Github account to set-up Live Share. 
+OK, I'll admit it. I've never used this feature, but I have tested it and it seems cool. You'll need a Github account to set-up the [Live Share extension in VS Code](https://code.visualstudio.com/learn/collaboration/live-share). 
